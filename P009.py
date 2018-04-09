152b0fe524bde7e60ece8e5d9be21adcc7f01e0b
Dios Malone <dios.malone@gmail.com>
2018-04-09 13:09:45 +0800

    Problem 9 is solved with refine

--- P009.py
+++ P009.py
@@ -2,11 +2,11 @@ import math
 
 def solution():
    for c in range(500, 1, -1):
-      for a in range(500):
-         for b in range(500):
-            if a + b + c == 1000 and a**2 + b**2 == c**2:
-               print("a={}, b={}, c={}".format(a,b,c))
-               return a*b*c
+      for a in range(1, 500):
+         b = 1000 - c - a 
+         if a + b + c == 1000 and a**2 + b**2 == c**2 and a != b and b!=c:
+            print("a={}, b={}, c={}".format(a,b,c))
+            return a*b*c
                
 print('Solution 1 result: {}'.format(solution()))
    
